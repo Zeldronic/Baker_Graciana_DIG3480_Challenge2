f65386f67954b06eb9d6d00b39eb87ca50315ad3
Zeldronic <60047948+Zeldronic@users.noreply.github.com>
2020-03-08 18:47:20 -0400

    Unity Challenge 2 for class

---
+++ PlatformandTilemapping-Tutorial2/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D rd2d;
+
+    public float speed;
+
+    public Text score;
+    public Text win;
+    public Text lifeText;
+
+    private int scoreValue = 0;
+    private int life;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+        score.text = scoreValue.ToString();
+        win.text = "";
+        life = 3;
+        SetLifeText();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+
+        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if (collision.collider.tag == "Coin")
+        {
+            scoreValue += 1;
+            score.text = scoreValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+
+        if (scoreValue == 4)
+        {
+            win.text = "You win! Game created by Graciana Baker";
+        }
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    void SetLifeText()
+    {
+        lifeText.text = "Lifes: " + life.ToString();
+
+        if (life == 0)
+        {
+
+            win.text = "Game Over.  Game created by Graci Baker.";
+            Destroy(this); //this destroys current controller -> destroys escape button input!! >:(
+
+        }
+
+    }
+}
